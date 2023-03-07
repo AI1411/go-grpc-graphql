ca8b46526a851d4c5fb46345fe33f1a1a645e1a3
AI1411 <goethe0719@gmail.com>
2023-03-08 06:23:56 +0900

    add tweet DDL and proto

---
+++ DDL/000005_create_tweets_table.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS tweets;
+CREATE TABLE IF NOT EXISTS tweets
+(
+    id         UUID                     NOT NULL DEFAULT gen_random_uuid(), -- ID
+    user_id    UUID                     NOT NULL,                           -- ユーザID
+    body       VARCHAR(255)             NOT NULL,                           -- 本文
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),             -- 作成日時
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()              -- 更新日時
+);
+
+COMMENT ON TABLE chats IS 'つぶやきテーブル';
+COMMENT ON COLUMN chats.id IS 'ID';
+COMMENT ON COLUMN chats.user_id IS 'ユーザID';
+COMMENT ON COLUMN chats.body IS '本文';
+COMMENT ON COLUMN chats.created_at IS '作成日時';
+COMMENT ON COLUMN chats.updated_at IS '更新日時';
+
+CREATE INDEX IF NOT EXISTS user_id_idx on chats (user_id);
+CREATE INDEX IF NOT EXISTS created_at_idx on chats (created_at);
--- Makefile
+++ Makefile
@@ -1,9 +1,5 @@
 .PHONY: up gen-user-proto
 up:
 	docker-compose --env-file .env.local -f docker-compose.yml up -d --build
-gen-user-proto:
-	protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --doc_out=./docs --doc_opt=html,user.html grpc/user.proto
-gen-room-proto:
-	protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --doc_out=./docs --doc_opt=html,room.html grpc/room.proto
-gen-chat-proto:
-	protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --doc_out=./docs --doc_opt=html,chat.html grpc/chat.proto
+gen-proto:
+	protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --doc_out=./docs --doc_opt=html,index.html grpc/*.proto
