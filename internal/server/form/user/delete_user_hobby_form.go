732fa94def45f1ba8e8c309dda4ae9e83dbaf65a
AI1411 <goethe0719@gmail.com>
2023-03-27 20:52:56 +0900

    fix form

--- internal/server/form/user/delete_user_hobby_form.go
+++ internal/server/form/user/delete_user_hobby_form.go
@@ -3,11 +3,13 @@ package user
 import "github.com/AI1411/go-grpc-graphql/grpc"
 
 type DeleteUserHobbyForm struct {
-	ID string `jaFieldName:"ID" validate:"required"`
+	UserID  string `jaFieldName:"ユーザID" validate:"required"`
+	HobbyID string `jaFieldName:"趣味ID" validate:"required"`
 }
 
 func NewDeleteUserHobbyForm(in *grpc.DeleteUserHobbyRequest) *DeleteUserHobbyForm {
 	return &DeleteUserHobbyForm{
-		ID: in.GetId(),
+		UserID:  in.GetUserId(),
+		HobbyID: in.GetHobbyId(),
 	}
 }
