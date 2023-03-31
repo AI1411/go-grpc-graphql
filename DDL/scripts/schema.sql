f67380cd180b245116354cbc268a33e3ce07510c
AI1411 <goethe0719@gmail.com>
2023-03-31 09:08:14 +0900

    reportsのDDL

--- DDL/scripts/schema.sql
+++ DDL/scripts/schema.sql
@@ -315,4 +315,25 @@ ALTER TABLE hobbies
             REFERENCES categories (id)
             ON DELETE CASCADE;
 
-CREATE INDEX categories_id_idx ON categories (id);
+CREATE INDEX categories_id_idx ON categories (id);DROP TABLE IF EXISTS reports CASCADE;
+CREATE TABLE reports
+(
+    id               UUID                  DEFAULT gen_random_uuid() PRIMARY KEY,
+    reporter_user_id UUID         NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    reported_user_id UUID         NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    reported_chat_id UUID         NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    reason           VARCHAR(255) NOT NULL,
+    created_at       TIMESTAMP    NOT NULL DEFAULT NOW(),
+    updated_at       TIMESTAMP    NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX reports_reporter_id_idx ON reports (reporter_user_id);
+CREATE UNIQUE INDEX reports_reported_user_id_reported_chat_id_idx ON reports (reporter_user_id, reported_chat_id);
+
+COMMENT ON TABLE reports IS '通報テーブル';
+COMMENT ON COLUMN reports.id IS '通報ID';
+COMMENT ON COLUMN reports.reporter_user_id IS '通報者ID';
+COMMENT ON COLUMN reports.reported_chat_id IS '通報されたチャットID';
+COMMENT ON COLUMN reports.reason IS '通報理由';
+COMMENT ON COLUMN reports.created_at IS '作成日時';
+COMMENT ON COLUMN reports.updated_at IS '更新日時';
